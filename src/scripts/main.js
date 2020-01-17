66d686f8a9a4648261f63ec6838bd570109216d3
castle <castlesmadeofcode@gmail.com>
2020-01-17 11:19:11 -0600

    fiexed src in html for mainjs, created objects to hold journal entries and an array to hold a collection of entries

---
+++ src/scripts/main.js
@@ -0,0 +1,32 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+
+const journalEntry = {
+
+};
+
+const journalEntryObjects = {
+
+};
+
+const journalEntryArrays = {
+
+};
+
+const journalEntryFunctions = {
+
+};
+
+const journalEntries = [];
+
+journalEntries.push(journalEntry);
+journalEntries.push(journalEntryObjects);
+journalEntries.push(journalEntryArrays);
+journalEntries.push(journalEntryFunctions);
+
+
+
+
+console.log("an array with 4 objects inside", journalEntries);
