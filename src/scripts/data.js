c209f7281721aa089c6960640ba40e84f50aee1f
castle <castlesmadeofcode@gmail.com>
2020-01-23 15:44:32 -0600

    completed Refactor, API Acess Module, and Main Application logic sections in Daily Journal 5. hit MVP for Daily Journal 5

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,6 +1,8 @@
 
-const fetchEntries = fetch("http://localhost:8088/entries") // Fetch from the API
-    .then(entries => entries.json())  // Parse as JSON
-    .then(parsedEntries => {
-        renderJournalEntries(parsedEntries);
-    })
+const API = {
+    getJournalEntries() {
+        return fetch("http://localhost:8088/entries")
+            .then(response => response.json())
+
+    }
+}
