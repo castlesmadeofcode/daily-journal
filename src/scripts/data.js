2fd69706997b7ad58e1912277bd77fb57a8eed7b
castle <castlesmadeofcode@gmail.com>
2020-02-03 14:58:49 -0600

    made code a bit more modular

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,13 +1,7 @@
 
 
+import makeJournal from "./entryComponent.js"
 
-
-const makeJournalEntryObject = (date, concept, entry, mood) => ({
-    "date": date,
-    "concept": concept,
-    "entry": entry,
-    "mood": mood
-});
 
 const API = {
     getJournalEntries() {
@@ -16,24 +10,23 @@ const API = {
 
     },
     saveJournalEntry() {
-        const newJournalEntry = makeJournalEntryObject(journalDate.value, journalConcepts.value, journalEntry.value, journalMood.value)
-       console.log(newJournalEntry)
-       if (journalDate.value !== "" && journalConcepts.value !== ""
-       && journalEntry.value !== "" && journalMood.value !== "") {
-        fetch("http://localhost:8088/entries", { // Replace "url" with your API's URL
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify(newJournalEntry)
-})
-  location.reload();
+        const newJournalEntry = makeJournal.makeJournalEntryObject(journalDate.value, journalConcepts.value, journalEntry.value, journalMood.value)
+        if (journalDate.value !== "" && journalConcepts.value !== ""
+            && journalEntry.value !== "" && journalMood.value !== "") {
+            fetch("http://localhost:8088/entries", { // Replace "url" with your API's URL
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newJournalEntry)
+            })
+            location.reload();
 
-}
-else {
-    window.alert("fill out all the things")
-}
-}
+        }
+        else {
+            window.alert("fill out all the things!")
+        }
+    }
 }
 
 
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,18 @@
 import API from "./data.js"
 import renderJournalEntries from "./entriesDOM.js"
-import makeJournalEntryObject from "./data.js"
+
+const journalDate = document.getElementById("journalDate");
+const journalConcepts = document.getElementById("journalConcepts");
+const journalEntry = document.getElementById("journalEntry");
+const journalMood = document.getElementById("journalMood");
+
 
 API.getJournalEntries().then(renderJournalEntries);
 
 
 const captureUserInfo = () => {
-
-    const journalDate = document.getElementById("journalDate");
-    const journalConcepts = document.getElementById("journalConcepts");
-    const journalEntry = document.getElementById("journalEntry");
-    const journalMood = document.getElementById("journalMood");
-
-
-    API.saveJournalEntry(makeJournalEntryObject);
-
-
+    API.saveJournalEntry();
 }
+
 document.getElementById("journalRecordEntry").addEventListener("click", captureUserInfo);
 
